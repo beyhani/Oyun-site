c0157768492cbb3a5a7b3dc805ab6eae4267c9b9
beyhani <camyesil04@gmail.com>
2026-01-12 02:35:02 +0300

    Add files via upload

---
+++ games.js
@@ -0,0 +1,17 @@
+const games = [
+  {
+    title:"Speed Car",
+    img:"https://img.gamedistribution.com/rvvASMiM-512x384.jpg",
+    embed:"https://html5.gamedistribution.com/rvvASMiM/"
+  },
+  {
+    title:"Action Hero",
+    img:"https://img.gamedistribution.com/4807350-512x384.jpg",
+    embed:"https://html5.gamedistribution.com/4807350/"
+  },
+  {
+    title:"Crazy Run",
+    img:"https://via.placeholder.com/512x384?text=Run",
+    embed:"https://example.com"
+  }
+];
